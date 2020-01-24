a7202dbd30485ac85a6a11d8b56f5022d1cdac86
Parsa Ghorbani <parsa.ghorbani@divar.ir>
2020-01-24 18:13:48 +0330

    Add update function to QLearner class

--- qlearner.py
+++ qlearner.py
@@ -1,4 +1,28 @@
+import operator
+import random
+
 class QLearner:
-    def __init__(self, move_request):
-        self.move_request = move_request
-        self.Q = {}
+    def __init__(self, move_request, criterion, initial_state, alpha, gamma, next_state):
+        self._move_request = move_request
+        self.criterion = criterion
+        self._previous_state = None
+        self._state = initial_state
+        self._Q = {self._state: {}}
+        self.alpha = alpha
+        self.gamma = gamma
+        self.next_state = next_state
+
+    def _get_max_action(self, state):
+        try:
+            return max(self._Q[state].items(), operator.itemgetter(1))[0]
+        except KeyError:
+            self._Q[state] = {}
+            return random.choice(self.next_state(self._state))
+
+    def _update(self, action, reward):
+        if self._state not in self._Q:
+            self._Q[self._state] = {}
+        self._Q[self._previous_state][action] = (1.0 - self.alpha) * self._Q[self._previous_state][action] + self.alpha * (reward + self._gamma * self._Q[self._state].get(self._get_max_action(self._state), 0))
+
+    
+        
