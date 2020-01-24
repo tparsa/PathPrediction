196da9ab870e9e3bc8cf194ee59a630217e82991
Parsa Ghorbani <parsa.ghorbani@divar.ir>
2020-01-24 21:19:55 +0330

    Add final main

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ from Criterions import get_cost_based_on_fuel, get_cost_based_on_time, get_cost_
 if __name__ == "__main__":
 
     universe = Universe(MapBuilder())
-    qlearner_1 = QLearner(universe.get_initial_state(), get_cost_based_on_fuel, universe.move_request, universe.get_terminal_state(), 1, 0.9, universe.next_state)
-    qlearner_2 = QLearner(universe.get_initial_state(), get_cost_based_on_time, universe.move_request, universe.get_terminal_state(), 1, 0.9,  universe.next_state)
-    qlearner_3 = QLearner(universe.get_initial_state(), get_cost_based_on_mixture, universe.move_request, universe.get_terminal_state(), 1, 0.9, universe.next_state)
+    qlearners = [QLearner(universe.get_initial_state(), get_cost_based_on_fuel, universe.move_request, universe.get_terminal_state(), 1, 0.9, universe.next_state)
+                , QLearner(universe.get_initial_state(), get_cost_based_on_time, universe.move_request, universe.get_terminal_state(), 1, 0.9,  universe.next_state)
+                , QLearner(universe.get_initial_state(), get_cost_based_on_mixture, universe.move_request, universe.get_terminal_state(), 1, 0.9, universe.next_state)]
 
 
     num_of_epochs = 1000
 
     for epoch_num in range(num_of_epochs):
-        while qlearner_1._state != universe.get_terminal_state():
-            qlearner_1.move()
-        # if epoch_num % 10 == 0:
-        #     print("Epoch num: {}".format(epoch_num))
-        qlearner_1.reset(universe.get_initial_state())
+        for qlearner in qlearners:
+            while qlearner._state != universe.get_terminal_state():
+                qlearner.move()
+            qlearner.reset(universe.get_initial_state())
 
-    print(qlearner_1._Q)
+    print("Energy:", qlearners[0]._Q, end='\n\n')
+    print("Time:", qlearners[1]._Q, end='\n\n')
+    print("E + T^2:", qlearners[2]._Q, end='\n\n')
